4defc9c703877077ea3edc9254be73d229e4f043
FairGameTCG <omarfhassam@gmail.com>
2026-08-03 17:40:46 -0600

    Update Cloudinary configuration with new credentials

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ app.config['UPLOAD_TIMEOUT'] = 30  # seconds
 
 # Configure Cloudinary
 cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
-    api_key=os.environ.get('CLOUDINARY_API_KEY', ''),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET', '')
+    cloud_name=os.environ.get('mjov8rmp', ''),
+    api_key=os.environ.get('224837851487556', ''),
+    api_secret=os.environ.get('wykJFpFPrTdWTGl-fMba13pZ3Q0', '')
 )
 
 # Pokédex mapping
